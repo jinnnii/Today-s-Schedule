28b9371e0f08011ca47224cc05f16e64ff0167a1
jinnnii <rdw_8690@naver.com>
2021-09-26 06:00:49 +0900

    Fix CSS Styling & ADD Today's date

--- script/main.js
+++ script/main.js
@@ -3,7 +3,12 @@ const input = document.querySelector('.footer_input');
 const tag = document.querySelector('.tag_select span');
 const addBtn = document.querySelector('.footer_button');
 
-let count = 0;
+let id = 0;
+
+function onApp() {
+    setDate();
+}
+onApp();
 
 function onAdd() {
     const text = input.value;
@@ -22,13 +27,6 @@ function onAdd() {
         msg.innerHTML = `<p></p>`;
     }
     const todo = createTodo(text, tagText);
-    const todoDelete = todo.querySelector('.todo_delete');
-    todoDelete.addEventListener('click', () => {
-        todo.classList.add('animate__animated', 'animate__fadeOut');
-        todo.addEventListener('animationend', () => {
-            todos.removeChild(todo);
-        });
-    });
     todos.appendChild(todo);
 
     todo.scrollIntoView({ block: 'center' });
@@ -38,27 +36,47 @@ function onAdd() {
 
 function createTodo(text, tagText) {
     const todoRow = document.createElement('li');
-    todoRow.setAttribute('class', 'item_row');
+    todoRow.setAttribute('class', 'todo_row');
+    todoRow.setAttribute('data-id', id);
 
     const todo = `
         <div class="todo animate__animated animate__fadeInUp">
-            <input type="checkbox" name="check" id="todo_${count}">
-            <label for="todo_${count}">
+            <input type="checkbox" name="check" id="todo_${id}">
+            <label for="todo_${id}">
                 <span class="todo_check"></span>
                 <div class="todo_tag_name">
                     <span class="todo_tag">${tagText}</span>
                     <span class="todo_name">${text}</span>
                 </div>
             </label>
-            <button class="todo_delete">
-                <i class="far fa-trash-alt"></i>
+            <button class="todo_delete hvr-pulse-shrink">
+                <i class="far fa-trash-alt" data-id="${id}"></i>
             </button>
         </div>
     `;
     todoRow.innerHTML = todo;
 
-    count++;
+    id++;
     return todoRow;
+}
+
+function onAddTag() {
+    const tag_input = document.querySelector('.input_tag');
+    const text = tag_input.value;
+}
+
+function setDate() {
+    let day_array = new Array('월', '화', '수', '목', '금', '토', '일');
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const date = today.getDate();
+    const day = day_array[today.getDay()];
+
+    document.querySelector('.center h3').innerHTML = `
+        ${year}.${month}.${date}.${day}요일
+    `;
+
 }
 
 addBtn.addEventListener('click', () => {
@@ -71,3 +89,15 @@ input.addEventListener('keypress', event => {
     }
 })
 
+todos.addEventListener('click', (event) => {
+    if (event.target.tagName === 'I') {
+        const id = event.target.dataset.id;
+        if (id) {
+            const toBeDelete = document.querySelector(`.todo_row[data-id="${id}"]`);
+            toBeDelete.classList.add('animate__animated', 'animate__fadeOut');
+            toBeDelete.addEventListener('animationend', () => {
+                todos.removeChild(toBeDelete);
+            });
+        }
+    }
+});
